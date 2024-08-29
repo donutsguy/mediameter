8e1187e373a2532d989613b1a780bb1fe655ff80
Leonardo Raposo <104743570+Leo-Raposo@users.noreply.github.com>
2024-08-29 01:50:03 -0300

    Update WatchedModel.java

--- src/main/java/com/tsystem/mediameter/models/WatchedModel.java
+++ src/main/java/com/tsystem/mediameter/models/WatchedModel.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 public class WatchedModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
