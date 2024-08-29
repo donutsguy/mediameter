87f00e02507b2926481fb746574e1240c30fcdc0
Leonardo Raposo <104743570+Leo-Raposo@users.noreply.github.com>
2024-08-29 01:48:35 -0300

    Update RatingModel.java

--- src/main/java/com/tsystem/mediameter/models/RatingModel.java
+++ src/main/java/com/tsystem/mediameter/models/RatingModel.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class RatingModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
     private Integer note;
