cd835de246d4d73126a13664cb9bdbb762aca1b5
Leonardo Raposo <104743570+Leo-Raposo@users.noreply.github.com>
2024-08-29 01:48:23 -0300

    Update PlatformModel.java

--- src/main/java/com/tsystem/mediameter/models/PlatformModel.java
+++ src/main/java/com/tsystem/mediameter/models/PlatformModel.java
@@ -23,7 +23,7 @@ public class PlatformModel implements Serializable {
     private static final long serialVersionUID = 1;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
